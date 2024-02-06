e05a5e6b3507246391fb5fbf25760a4c2ba04f7a
Novik Dmytro <dinovik@rambler.ru>
2024-02-06 03:21:47 +0300

    fix: fix bugs with cd

--- src/files/rn.js
+++ src/files/rn.js
@@ -3,9 +3,9 @@ import path from 'path';
 import { operationFailed } from '../utils/consoleMessages.js';
 
 const rn = async (directory, args) => {
-  const filePath = path.isAbsolute(args[0]) ? args[0] : path.resolve(directory, args[0]);
-  const newFileName = path.resolve(path.parse(filePath).dir, args[1])
-    fs.rename(filePath, path.resolve(filePath, newFileName), (error) => {
+  const oldPath = path.isAbsolute(args[0]) ? args[0] : path.resolve(directory, args[0]);
+  const newPath = path.resolve(path.parse(oldPath).dir, args[1])
+    fs.rename(oldPath, path.resolve(oldPath, newPath), (error) => {
       if (error) {
         console.log(error);
         operationFailed();
--- src/navigation/cd.js
+++ src/navigation/cd.js
@@ -1,9 +1,16 @@
 import path from 'path';
+import fs from 'fs/promises';
 
-const cd = (directory, args) => {
-  const destPath = args[0];
-
-  return path.isAbsolute(destPath) ? destPath : path.resolve(directory, destPath);
+const cd = async (directory, args) => {
+  const name = args[0] || '';
+  const folderName = path.isAbsolute(name) ? name : path.resolve(directory, name);
+  const folderData = await fs.readdir(directory, 'utf-8');
+  console.log(folderData, folderName);
+  const item = folderData.find((value) => value === name);
+  console.log(item);
+  if (item && (await fs.stat(path.resolve(directory, item))).isDirectory()) {
+    return folderName;
+  } else return directory;
 }
 
 export default cd;
